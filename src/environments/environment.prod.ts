b9a99095dbca8b1ffec072f5dbb166542217d8e8
Tsiory Andrianarisaona <tsiory.andrianarisaona@hotmail.com>
2022-04-29 20:28:10 +0300

    deploiement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_url: " https://mbds-madagascar-2022-api.herokuapp.com/api/"
+  api_url: "https://mbds-madagascar-2022-api.herokuapp.com/api/"
 };
